966d4d8eae55b63c2b5c464cab0f4921a0c3f00b
ishaan fas <you@example.com>
2020-07-11 16:38:03 +0530

    first-commit

---
+++ jscript.js
@@ -0,0 +1,10 @@
+function ha() {
+  document.getElementById("menu_section").style.height = "150vw";
+  document.getElementById("hehe").style.display = "block";
+  document.getElementById("hehe").style.opacity = "1";
+
+  var vid = document.getElementById("hehe");
+  vid.autoplay = true;
+  vid.load();
+  console.log();
+}
